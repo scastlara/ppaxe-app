9b19138aa46b3ddd80f674c90c62505b36c302b9
Sergio Castillo <s.cast.lara@gmail.com>
2018-04-20 12:54:37 +0200

    tutorial done

--- static/back-to-top.js
+++ static/back-to-top.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($){
 	// browser window scroll (in pixels) after which the "back to top" link is shown
 	var offset = 300,
 		//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
-		offset_opacity = 1200,
+		offset_opacity = 800,
 		//duration of the top scrolling animation (in ms)
 		scroll_top_duration = 700,
 		//grab the "back to top" link
