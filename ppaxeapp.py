42bc014e85cf5551ca85583097f6ae8ca7e9d15c
scastlara <s.cast.lara@gmail.com>
2018-10-10 14:47:11 +0200

    EXPERIMENTAL: Job system.

--- ppaxeapp.py
+++ ppaxeapp.py
@@ -9,7 +9,6 @@ from ppaxe import PubMedQueryError
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 from xhtml2pdf import pisa
-from StringIO import StringIO
 from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -22,6 +21,15 @@ import requests
 from xml.dom import minidom
 from joblib import Parallel, delayed
 from timeit import default_timer as timer
+from StringIO import StringIO
+import threading
+import random
+import cPickle as pickle
+import sqlite3
+import datetime
+import time
+import uuid
+
 
 class ReverseProxied(object):
     '''Wrap the application in this middleware and configure the 
@@ -88,6 +96,14 @@ def create_pdf(pdf_data):
     pisa.CreatePDF(StringIO(pdf_data), pdf)
     return pdf
 
+
+def connect_to_db():
+    '''
+    Returns connection to db
+    '''
+    db = sqlite3.connect('mydb')
+    return db
+
 # -----------------
 def send_mail(send_from, send_to, subject, attachment=None):
     '''
@@ -117,114 +133,195 @@ def parallel_predict_interactions(sentence):
     return sentence
 
 
+def init_job(db, job):
+    """
+    Initializes job in database
+    """
+    start_time = datetime.datetime.now()
+    db.execute(
+        "INSERT into jobs(id, date, updated, progress) VALUES (?, ?, ?, ?)", 
+        (job, start_time, start_time, 0) 
+    )
+    db.commit()
+    return start_time
+
+
+def update_progress(db, job, progress):
+    """
+    Updates progress of job in database
+    """
+    db.execute("""
+        UPDATE jobs
+        SET progress = ?, updated = ?
+        WHERE id = ?
+    """, (progress, datetime.datetime.now(), job) )
+    db.commit()
+
+def get_progress(db, job):
+    """
+    Returns progress for a job
+    """
+    cursor = db.cursor()
+    cursor.execute("SELECT progress from jobs WHERE id = ?", (job,))
+    result = cursor.fetchone()
+    return result[0]
+
+
+def create_response(summary, query):
+    """
+    Creates response object out of the summary.
+    """
+    response = dict()
+    response['nints']  = summary.graphsummary.numinteractions
+    response['nprots'] = summary.protsummary.totalprots
+
+    if response['nprots'] > 0:
+        response['prot_table'] = summary.protsummary.table_to_html()
+    else:
+        response['nprots'] = 0
+
+    if query.database == "PUBMED":
+        response['source'] = "Abstracts"
+    else:
+        response['source'] = "Full texts"
+        
+    if response['nints'] > 0:
+        # Tables
+        response['int_table'] = summary.graphsummary.table_to_html()
+        response['sum_table'] = summary.summary_table()
+        # JSON graph
+        response['graph'] = summary.graphsummary.graph_to_json()
+        # Plots
+        response['plots'] = dict()
+        response['plots']['j_int_plot'], response['plots']['j_prot_plot'], response['plots']['a_year_plot'] = summary.journal_plots()
+        response['plots']['j_prot_plot'] = response['plots']['j_prot_plot'].getvalue().encode("base64").strip()
+        response['plots']['j_int_plot']  = response['plots']['j_int_plot'].getvalue().encode("base64").strip()
+        response['plots']['a_year_plot'] = response['plots']['a_year_plot'].getvalue().encode("base64").strip()
+        response['today'] = datetime.date.today()
+        response['database'] = query.database
+        with app.app_context():
+            response['pdf-plain'] = create_pdf(render_template('pdf.html', identifiers=query.ids, response=response))
+        response['pdf'] = "data:application/pdf;base64," + base64.b64encode(response['pdf-plain'].getvalue())
+    return response
+
+
+# THREADING
+# -----------------------------------------------------------------------
+class ExportingThread(threading.Thread):
+    def __init__(self, job_id, query, source):
+        super(ExportingThread, self).__init__()
+        self.job_id = job_id
+        self.query = query
+        self.source = source
+
+    def run(self):
+        # WHAT TO DO:
+        #   - Save to database only the Job data (job identifier, job date)
+        #   - Save pickle with query object.
+        #   - When requesting the /job?id=XXX query database, if it is marked as "done", 
+        #     unpickle the query and do the summary.
+        #   - Instead of pickling the Query, we can make the summary, create the "response"
+        #     dictionary and then pickling that, saving a lot of space.
+        # Initialize Job
+        db = connect_to_db()
+        start_time = init_job(db, self.job_id)
+
+        # Get Articles
+        self.query.get_articles()
+        update_progress(db, self.job_id, 1)
+
+        # Predict Interactions
+        for article in self.query:
+            article.extract_sentences(source=self.source)
+            sentences = [ sentence.annotate() for sentence in article.sentences ]
+            article.sentences = Parallel(n_jobs=NJOBS, backend="loky", verbose=int(os.environ.get('PPAXE_DEBUG', 0)))(delayed(parallel_predict_interactions)(sentence) for sentence in article.sentences)
+        update_progress(db, self.job_id, 2)
+        
+        # Make Summary
+        summary = report.ReportSummary(self.query)
+        summary.graphsummary.makesummary()
+        summary.protsummary.makesummary()
+        update_progress(db, self.job_id, 3)
+
+        # Create response object
+        response = create_response(summary, self.query)
+        response['start_time'] = start_time
+        pickle_file = "tmp/%s_results.pkl" % self.job_id
+        with open(pickle_file, 'wb') as p_fh:
+            pickle.dump(response, p_fh)   
+        update_progress(db, self.job_id, 4)
+
+
 # VIEWS
 # -----------------------------------------------------------------------
 @app.route('/', methods=['GET', 'POST'])
 def home_form():
-    '''
-    Home of the ppaxe web application containing the form
-    '''
     identifiers = str()
     database    = str()
     response    = dict()
     response['search'] = False
-
+    template = "base.html"
     if 'identifiers' in request.form:
-        # Get Form parameters
         response['search'] = True
-        response['njobs']  = NJOBS
+        template = "progress.html"
+        job_id = int(str(int(time.time())) + str(random.randint(0, 100)))
+        print(job_id)
+        response['job_id'] = job_id
         identifiers = request.form['identifiers']
         database = request.form['database']
-        response['database'] = database
-        email = request.form['email']
         identifiers = re.split(",|\n|\r", identifiers)
-        identifiers = [ident for ident in identifiers if ident]
+        identifiers = [ ident for ident in identifiers if ident ]
+        if database == "PUBMED":
+            source = "abstract"
+        else:
+            source = "fulltext"
+        query = core.PMQuery(ids=identifiers, database=database)
+        thread = ExportingThread(job_id, query, source)
+        thread.start()
+        # render progress template
+        # which will do a jquery async query to /progess?job=XXXXX
+        # Once progress == 3: redirect to /results?job=XXXX
+    return render_template(template, identifiers=identifiers, response=response)
 
-        # Get articles from Pubmed or PMC
-        source = str()
-        start_download = timer()
-        try:
-            query = core.PMQuery(ids=identifiers, database=database)
-            query.get_articles()
-        except PubMedQueryError:
-            response['error'] = "PMQuery"
-        end_download = timer()
-        time_download = (end_download - start_download)
 
-        # Retrieve interactions from 'source'
-        try:
-            if database == "PUBMED":
-                source = "abstract"
-            else:
-                source = "fulltext"
-            response['source'] = source.title()
+@app.route('/job/<int:job_id>')
+def job(job_id):
+    '''
+    Allows users to check the progress of their job.
+    '''
+    db = connect_to_db()
+    progress = get_progress(db, job_id)
+    response = dict()
+    template = "progress.html"
+    if progress < 4:
+        # Not done yet
+        # Redirect to progress page
+        response['search'] = True
+        response['job_id'] = job_id
+    else:
+        # Done
+        # Redirect to results page
+        template = "results.html"
+        pickle_file = "tmp/%s_results.pkl" % job_id
+        with open(pickle_file, 'rb') as p_fh:
+            response = pickle.load(p_fh)
+        response['search'] = True
+        response['job_id'] = job_id
+    return render_template(template, response=response)
 
-            time_corenlp = 0
-            time_analysis = 0
-            for article in query.articles:
-                article.extract_sentences(source=source)
-                start_corenlp = timer()
-                sentences = [ sentence.annotate() for sentence in article.sentences ]
-                end_corenlp = timer()
-                time_corenlp += (end_corenlp - start_corenlp)
-                start_analysis = timer()
-                article.sentences = Parallel(n_jobs=NJOBS, backend="loky", verbose=int(os.environ.get('PPAXE_DEBUG', 0)))(delayed(parallel_predict_interactions)(sentence) for sentence in article.sentences)
-                end_analysis = timer()
-                time_analysis +=(end_analysis - start_analysis)
-            start_report = timer()
-            summary = report.ReportSummary(query)
-            summary.graphsummary.makesummary()
-            summary.protsummary.makesummary()
-            response['nints']  = summary.graphsummary.numinteractions
-            response['nprots'] = summary.protsummary.totalprots
 
-            if response['nprots'] > 0:
-                response['prot_table'] = summary.protsummary.table_to_html()
-            else:
-                response['nprots'] = 0
-
-            if response['nints'] > 0:
-                # Tables
-                response['int_table'] = summary.graphsummary.table_to_html()
-                response['sum_table'] = summary.summary_table()
-                # JSON graph
-                response['graph'] = summary.graphsummary.graph_to_json()
-                # Plots
-                response['plots'] = dict()
-                response['plots']['j_int_plot'], response['plots']['j_prot_plot'], response['plots']['a_year_plot'] = summary.journal_plots()
-                response['plots']['j_prot_plot'] = response['plots']['j_prot_plot'].getvalue().encode("base64").strip()
-                response['plots']['j_int_plot']  = response['plots']['j_int_plot'].getvalue().encode("base64").strip()
-                response['plots']['a_year_plot'] = response['plots']['a_year_plot'].getvalue().encode("base64").strip()
-                response['today'] = datetime.date.today()
-                response['database'] = database
-                response['pdf-plain'] = create_pdf(render_template('pdf.html', identifiers=identifiers, response=response))
-                response['pdf'] = "data:application/pdf;base64," + base64.b64encode(response['pdf-plain'].getvalue())
-                if email:
-                    server = smtplib.SMTP('smtp.gmail.com', 587)
-                    server.starttls()
-                    server.login(os.environ.get('PPAXE_EUSER', "dummy"), os.environ.get('PPAXE_EPASSW', "ymmud"))
-                    msg = send_mail(os.environ.get('PPAXE_EMAIL', "dummy@email.com"), email, 'PPaxe results', response['pdf-plain'])
-                    server.sendmail(os.environ.get('PPAXE_EMAIL', "dummy@email.com"), email, msg.as_string())
-
-            end_report = timer()
-            time_report = (end_report - start_report)
-
-            # Save execution time for different processes
-            response['time_download'] = round(time_download, 3)
-            response['time_corenlp'] = round(time_corenlp, 3)
-            response['time_analysis'] = round(time_analysis, 3)
-            response['time_report']   = round(time_report, 3)
-
-        except Exception as err:
-            debugmsg = int(os.environ.get('PPAXE_DEBUG', 0))
-            if debugmsg:
-                response['debug'] = True
-                response['server-error'] = err
-            else:
-                response['debug'] = False
-                response['server-error'] = "Internal Server Error"
-
-    return render_template('home.html', identifiers=identifiers, response=response)
+@app.route('/progress/<int:job_id>')
+def progress(job_id):
+    '''
+    Returns a string with the progress
+        1 : Articles downloaded
+        2 : Interactions retrieved
+        3 : Report ready
+        4 : Results ready
+    '''
+    db = connect_to_db()
+    progress = get_progress(db, job_id)
+    return str(progress)
 
 
 @app.route('/querypubmed', methods=['GET'])
